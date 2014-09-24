966a64fb843ff30e427f996864837544e7bda969
AileOrgla <aileorgla@gmail.com>
2014-09-24 18:26:11 +0300

    Add tags support

--- controllers/posts.php
+++ controllers/posts.php
@@ -5,12 +5,16 @@ class posts extends Controller{
     function view (){
         $post_id = $this->params[0];
         $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
+        $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
 }
 
 
 function index(){
-		$this->users = get_all("SELECT * FROM user");
         $this->posts = get_all("SELECT * FROM post");
+        $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
+        foreach ($_tags as $tag) {
+            $this->tags[$tag['post_id']][] = $tag;
+        }
 	}
 
     function index_ajax(){
