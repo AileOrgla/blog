961f90b89051990f941acbd504c31bdc0eb3bd93
AileOrgla <aileorgla@gmail.com>
2014-10-06 16:15:49 +0300

    Update database

---
+++ views/posts/posts_view.php
@@ -0,0 +1,25 @@
+<div class="span8">
+    <h1><?= $post['post_subject'] ?></h1>
+
+    <p><?= $post['post_text'] ?></p>
+
+    <div>
+        <? foreach ($tags as $tag): ?>
+            <a href="tags/view/<?= $tag['tag_name'] ?>">
+                <span class="label label-info"><?= $tag['tag_name'] ?></span>
+            </a>
+        <? endforeach ?>
+        <p><span class="badge"><?= $post['post_created'] ?></span>
+        </p></div>
+</div>
+<h3>Kommentaarid</h3>
+<? foreach ($comments as $comment): ?>
+    <div class="well">
+        <p><?= $comment['comment_text'] ?></p>
+
+        <p><?= $comment['comment_created'] ?></p>
+
+        <p><?= $comment['username'] ?></p>
+    </div>
+<? endforeach ?>
+
